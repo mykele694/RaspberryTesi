2de082d0a6956fb4fc22d4c69d2201cb6c6c998b
mykele694 <48528171+mykele694@users.noreply.github.com>
2019-04-05 17:46:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from qr import QR_class
+from acq_camera import VideoCam
+from tracker import Tracker_class
+
+# Creo oggetti
+CAM = VideoCam()
+QR_OBJ = QR_class()
+TRACK_OBJ = Tracker_class()
+
+#inizializzo 
+
+QR_OBJ.set_CAM_obj(CAM)
+TRACK_OBJ.set_CAM_obj(CAM)
+TRACK_OBJ.set_QR_obj(QR_OBJ)
+
+# Avvio thread
+CAM.start()
+QR_OBJ.start()
+TRACK_OBJ.start()
+
+#Robot_def = Robot()
+
+# Inizializzo oggetti
+#self.Tracker_obj.set_obj(Robot_def)
+
+# Avvio i Thread
+
+# Avvio la macchina principale
