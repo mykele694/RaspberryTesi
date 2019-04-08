c410f8e1ce862002f06315a3276fb436789e8b7a
mykele694 <48528171+mykele694@users.noreply.github.com>
2019-04-08 18:29:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from qr import QR_class
 from acq_camera import VideoCam
 from tracker import Tracker_class
-
+from robot import Robot
 # Creo oggetti
 CAM = VideoCam()
 QR_OBJ = QR_class()
 TRACK_OBJ = Tracker_class()
-
+ROBOT_OBJ = Robot()
 #inizializzo 
 
 QR_OBJ.set_CAM_obj(CAM)
 TRACK_OBJ.set_CAM_obj(CAM)
 TRACK_OBJ.set_QR_obj(QR_OBJ)
-
+ROBOT_OBJ.set_obj_cam (CAM)
 # Avvio thread
 CAM.start()
 QR_OBJ.start()
 TRACK_OBJ.start()
-
+ROBOT_OBJ.start()
 #Robot_def = Robot()
 
 # Inizializzo oggetti
@@ -26,3 +26,4 @@ TRACK_OBJ.start()
 # Avvio i Thread
 
 # Avvio la macchina principale
+
